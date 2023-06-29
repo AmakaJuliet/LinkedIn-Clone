7d0b6275e83deda333113b58b1fbaed90cb05f4d
Williams Afiukwa <62770751+w-liamx@users.noreply.github.com>
2023-06-29 17:17:02 +0200

    Update image.storage.ts

--- src/auth/helpers/image.storage.ts
+++ src/auth/helpers/image.storage.ts
@@ -1,61 +1,80 @@
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
-
-const fs = require('fs');
-import FileType from 'file-type';
-import { fileTypeFromFile } from 'file-type';
-import * as nodeFetch from "node-fetch"
-
-import path = require('path');
+import FileType, { FileExtension, MimeType } from 'file-type';
 import { Observable, from, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators'
+import { switchMap } from 'rxjs/operators';
+import path from 'path';
+import fs from 'fs';
 
 type validFileExtension = 'png' | 'jpg' | 'jpeg';
 type validMimeType = 'image/png' | 'image/jpg' | 'image/jpeg';
 
-const validFileExtensions: validFileExtension[] = ['png' ,'jpg' ,'jpeg'];
-const validMimeTypes: validMimeType[] = [
-    'image/png',
-    'image/jpg',
-    'image/jpeg',
+const validFileExtensions: validFileExtension[] | FileExtension[] = [
+  'png',
+  'jpg',
+  'jpeg',
+];
+const validMimeTypes: validMimeType[] | MimeType[] = [
+  'image/png',
+  'image/jpg',
+  'image/jpeg',
 ];
 
 export const saveImageToStorage = {
-    storage: diskStorage({
-      destination: './images',
-      filename: (req, file, cb) => {
-        const fileExtension: string = path.extname(file.originalname);
-        const fileName: string = uuidv4() + fileExtension; 
-        cb(null, fileName)
-      }
-    }),
-    fileFilter: (req, file, cb) => {
-      const allowedMimeTypes: validMimeType[] = validMimeTypes;
-      allowedMimeTypes.includes(file.mimetype) ? cb(null, true) : cb(null, false);
-    }
-}
+  storage: diskStorage({
+    destination: './images',
+    filename: (req, file, cb) => {
+      const fileExtension: string = path.extname(file.originalname);
+      const fileName: string = uuidv4() + fileExtension;
+      cb(null, fileName);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const allowedMimeTypes: validMimeType[] = validMimeTypes;
+    allowedMimeTypes.includes(file.mimetype) ? cb(null, true) : cb(null, false);
+  },
+};
+
+// export const isFileExtensionSafe_old = (
+//   fullFilePath: string,
+// ): Observable<boolean> => {
+//   return from(FileType.fileTypeFromFile(fullFilePath)).pipe(
+//     switchMap(
+//       (fileExtensionAndMimeType: {
+//         ext: validFileExtension;
+//         mime: validMimeType;
+//       }) => {
+//         if (!fileExtensionAndMimeType) return of(false);
+
+//         const isFileTypeLegit = validFileExtensions.includes(
+//           fileExtensionAndMimeType.ext,
+//         );
+//         const isMimeTypeLegit = validMimeTypes.includes(
+//           fileExtensionAndMimeType.mime,
+//         );
+//         const isFileLegit = isFileTypeLegit && isMimeTypeLegit;
+//         return of(isFileLegit);
+//       },
+//     ),
+//   );
+// };
 
 export const isFileExtensionSafe = (
   fullFilePath: string,
 ): Observable<boolean> => {
   return from(FileType.fileTypeFromFile(fullFilePath)).pipe(
-    switchMap(
-      (fileExtensionAndMimeType: {
-        ext: validFileExtension;
-        mime: validMimeType;
-      }) => {
-        if (!fileExtensionAndMimeType) return of(false);
+    switchMap((fileExtensionAndMimeType) => {
+      if (!fileExtensionAndMimeType) return of(false);
 
-        const isFileTypeLegit = validFileExtensions.includes(
-          fileExtensionAndMimeType.ext,
-        );
-        const isMimeTypeLegit = validMimeTypes.includes(
-          fileExtensionAndMimeType.mime,
-        );
-        const isFileLegit = isFileTypeLegit && isMimeTypeLegit;
-        return of(isFileLegit);
-      },
-    ),
+      const isFileTypeLegit = validFileExtensions.includes(
+        fileExtensionAndMimeType.ext as validFileExtension,
+      );
+      const isMimeTypeLegit = validMimeTypes.includes(
+        fileExtensionAndMimeType.mime as validMimeType,
+      );
+      const isFileLegit = isFileTypeLegit && isMimeTypeLegit;
+      return of(isFileLegit);
+    }),
   );
 };
 
@@ -65,4 +84,4 @@ export const removeFile = (fullFilePath: string): void => {
   } catch (err) {
     console.error(err);
   }
-}
+};
