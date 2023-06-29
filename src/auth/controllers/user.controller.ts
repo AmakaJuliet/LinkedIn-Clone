3dc3f0395c42ac9e180554e4f321f725ac278bb0
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2023-06-29 18:04:51 +0100

    commit

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -5,13 +5,17 @@ import {
   Request, 
   UseInterceptors, 
   UploadedFile,
+  Res,
+  Get,
 } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { JwtGuard } from '../guards/jwt.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { saveImageToStorage } from '../helpers/image.storage';
-import { of } from 'rxjs';
+import { isFileExtensionSafe, saveImageToStorage, removeFile } from '../helpers/image.storage';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { join } from 'path';
+import { UpdateResult } from 'typeorm';
 
 @Controller('user')
 export class UserController {
@@ -20,7 +24,7 @@ export class UserController {
   @UseGuards(JwtGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', saveImageToStorage))
-  uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): any {
+  uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): Observable<UpdateResult | { error: string} > {
     const fileName = file?.filename;
 
     if (!fileName) return of({ error: 'File must be a png, jpg/jpeg'});
@@ -28,6 +32,26 @@ export class UserController {
     const imagesFolderPath = join(process.cwd(), 'images');
     const fullImagePath = join(imagesFolderPath + '/' + file.filename);
 
-    return of({ error: 'File content does not match extension!' })
+    return isFileExtensionSafe(fullImagePath).pipe(
+      switchMap((isFileLegit: boolean) => {
+        if (isFileLegit) {
+          const userId = req.user.id;
+          return this.userService.updateUserImageById(userId, fileName);
+        }
+        removeFile(fullImagePath);
+        return of({ error: 'File content does not match extension!' })
+      }),
+    );
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('image')
+  findImage(@Request() req, @Res() res): Observable<Object> {
+    const userId = req.user.id;
+    return this.userService.findImageNameByUserId(userId).pipe(
+      switchMap((imageName: string) => {
+        return of(res.sendFile(imageName, { root: './images' }))
+      }),
+    );
   }
 }
